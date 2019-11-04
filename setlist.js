dcc660d3c89b274fd91f8f6acab09b3a1af06cda
Olivia Ng <43549231+olivialng@users.noreply.github.com>
2019-11-03 20:45:31 -0500

    Add files via upload

---
+++ setlist.js
@@ -0,0 +1,72 @@
+songs = [
+{	"id": 0, 
+	"Title": "You Belong With Me", 
+	"Artist": ["Taylor Swift"],
+	"Genre": ["Country", "Pop"],
+	"Year": 2008
+},
+{	
+	"id": 1, 
+	"Title": "Dont Stop Believin", 
+	"Artist": ['Journey'],
+	"Genre": ['Rock'],
+	"Year": 1981
+},
+{	
+	"id": 2, 
+	"Title": 'You and I', 
+	"Artist": ['Ingrid Michaelson'],
+	"Genre": ['Indie', 'Alternative'],
+	"Year": 2006
+},
+{	
+	"id": 3, 
+	"Title": 'Party in the USA', 
+	"Artist": ['Miley Cyrus'],
+	"Genre": ['Pop'],
+	"Year": 2009
+},
+{	
+	"id": 4, 
+	"Title": 'My Shot', 
+	"Artist": ['Lin Manuel Miranda'],
+	"Genre": ['Musical'],
+	"Year": 2015
+},
+{	
+	"id": 5, 
+	"Title": 'Castle on the Hill', 
+	"Artist": ['Ed Sheeran'],
+	"Genre": ['Pop'],
+	"Year": 2016
+},
+{	
+	"id": 6, 
+	"Title": 'Love Story', 
+	"Artist": ['Taylor Swift'],
+	"Genre": ['Country', 'Pop'],
+	"Year": 2008
+},
+{	
+	"id": 7, 
+	"Title": 'Landslide', 
+	"Artist": ['Fleetwood Mac'],
+	"Genre": ['Rock', 'Pop'],
+	"Year": 1975
+},
+{	
+	"id": 8, 
+	"Title": 'Banana Pancakes', 
+	"Artist": ['Jack Johnson'],
+	"Genre": ['Singer-Songwriter'],
+	"Year": 2005
+},
+{	
+	"id": 9, 
+	"Title": 'Just the Way You Are', 
+	"Artist": ['Bruno Mars'],
+	"Genre": ['Pop'],
+	"Year": 2010
+}
+]
+
